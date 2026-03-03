c5f0d72456a8499ae2056334aff5f13169502f4f
Matthew Song <matthewsong18@gmail.com>
2026-03-02 19:15:51 -0500

    test(storage): can save a routine to memory

--- storage/tests/storage_tests.rs
+++ storage/tests/storage_tests.rs
@@ -1,4 +1,4 @@
-use domain::routine_repository::RoutineRepository;
+use domain::{routine::Routine, routine_repository::RoutineRepository};
 use storage::memory_routine_repository::MemoryRoutineRepository;
 
 macro_rules! generate_storage_tests {
@@ -11,6 +11,16 @@ macro_rules! generate_storage_tests {
                 async fn test_default() {
                     let repo = $setup;
                     assert_eq!(0, repo.get_all().await.unwrap().len());
+                }
+
+                #[tokio::test]
+                async fn test_save_routine() {
+                    let repo = $setup;
+                    assert_eq!(0, repo.get_all().await.unwrap().len());
+
+                    let new_routine = Routine::new();
+                    repo.save(new_routine).await.unwrap();
+                    assert_eq!(1, repo.get_all().await.unwrap().len());
                 }
             }
         )*
